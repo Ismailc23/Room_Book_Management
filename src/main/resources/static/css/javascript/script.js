4b5e4b65add6f3c4c4b3165a1e165c1278164791
Ismailc23 <mohammed.chenoth@peopl10.com>
2024-06-28 00:18:42 +0530

    Minor modification done on UI part

--- src/main/resources/static/css/javascript/script.js
+++ src/main/resources/static/css/javascript/script.js
@@ -1,8 +1,4 @@
 function dateValidation() {
-    const errorMessageDiv = document.getElementById('error-message');
-    errorMessageDiv.style.display = 'none';
-    errorMessageDiv.innerText = '';
-
     const startDateInput = document.getElementById('stayStartDate').value;
     const endDateInput = document.getElementById('stayEndDate').value;
 
@@ -12,33 +8,24 @@ function dateValidation() {
 
 
     if (!startDateInput || !endDateInput) {
-        showError('Both start date and end date are required.');
         event.preventDefault();
-        return;
+        alert("Both start date and end date are required.");
     }
 
     if (startDate < currentDate.setHours(0, 0, 0, 0)) {
-        showError('Stay start date cannot be in the past.');
         event.preventDefault();
-        return;
+        alert("Stay start date cannot be in the past.");
     }
 
     if (endDate < startDate) {
-        showError('Stay end date cannot be before start date.');
         event.preventDefault();
-        return;
+        alert("End date should not be before Start Date");
     }
 
     const daysBetween = (endDate - startDate) / (1000 * 60 * 60 * 24);
     if (daysBetween > 30) {
-        showError('Maximum stay duration is 30 days.');
         event.preventDefault();
-        return;
+        alert("Maximum stay duration is 30 days.");
     }
 }
 
-function showError(message) {
-    const errorMessageDiv = document.getElementById('error-message');
-    errorMessageDiv.style.display = 'block';
-    errorMessageDiv.innerText = message;
-}
